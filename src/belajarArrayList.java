8d7362f157a87f913b60ffb00db3ab37bc10f796
RAFLINUR <145580383+RAFLINUR@users.noreply.github.com>
2024-01-15 10:53:45 +0700

    belajarArray

--- src/belajarArrayList.java
+++ src/belajarArrayList.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class belajarArrayList {
+public class belajarArrayList {
+    public static void main(String[] args) {
+      //misal terdapat value 4,18,15,35 akan dimasukan kedalam
+      //arrayList bertipe data integer
+
+      //deklarasi
+
+
+    }
 }
